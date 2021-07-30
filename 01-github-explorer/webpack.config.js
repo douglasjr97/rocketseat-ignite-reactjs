9dd4256b54bb2a8778b5533f1c77bb58be07f577
Douglas Falcao <falcao.douglas97@gmail.com>
2021-07-29 22:00:45 -0300

    Tudo certo com o Bundle Automatico

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -14,6 +15,11 @@ module.exports = {
     devServer: {
         contentBase: path.resolve(__dirname, 'public'),
     },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
     module:{
         rules: [
             {
