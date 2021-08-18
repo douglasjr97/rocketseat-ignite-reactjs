a9d44cc7572a8340b9d202b38eee4281658e19ca
Douglas Falcao <falcao.douglas97@gmail.com>
2021-08-18 00:12:58 -0300

    State

--- testes/src/components/counterState/contador.tsx
+++ testes/src/components/counterState/contador.tsx
@@ -2,11 +2,25 @@ import { useState } from 'react';
 
 export function Contador(){
     const [count, setCount] = useState(10)
+
+    const handleMinusClick = () =>{
+        if(count > 0){
+            setCount(count -1)
+        }
+    };
+
+    const handlePlusClick = () =>{
+        if(count < 10){
+            setCount(count +1)
+        }
+    };
+
     return(
     <>
         <h1>Contador: {count}</h1>
-        <button className="minus" onClick={() => setCount(count-1)}>-</button>
-        <button onClick={() => setCount(count +1)}>+</button>
+        <button className="minus"
+         onClick={handleMinusClick}>-</button>
+        <button onClick={handlePlusClick}>+</button>
         
 </>
     );
